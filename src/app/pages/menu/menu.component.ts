b0f89a0a36cc00b10b2e816884f1ab3179e76d47
dduygubaran <duygubaran1995@gmail.com>
2024-05-06 15:23:51 +0300

    Menupage düzenlendi.

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  constructor(private service: OrderDetailsService) {}
+
+  displayFood : any = [];
+
+  ngOnInit(): void {
+    this.foodSelection();
+  }
+
+
+  foodSelection() {
+    this.service.foodService().subscribe((data) => {
+      console.log(data);
+      this.displayFood = data. categories;
+    })
+  }
 
 }
