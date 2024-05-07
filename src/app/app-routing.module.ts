f4a9e52237bdfe0d15f3b93c71e59abbfd0c75e9
Duygu Baran <83429611+dduygubaran@users.noreply.github.com>
2024-05-07 13:44:21 +0300

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { MenuPageComponent } from './pages/menu-page/menu-page.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
